44bc02b47a9bb8579ff725896f8c727cb5b10d49
François Colas <francoiscolas@gmail.com>
2011-11-29 09:57:22 +0100

    Replace jsdoc template with codeview.

--- templates/codeview/publish.js
+++ templates/codeview/publish.js
@@ -4,8 +4,15 @@ function publish(symbolSet) {
 		ext:         ".html",
 		outDir:      JSDOC.opt.d || SYS.pwd+"../out/jsdoc/",
 		templatesDir: JSDOC.opt.t || SYS.pwd+"../templates/jsdoc/",
+		staticDir:   "static/",
 		symbolsDir:  "symbols/",
-		srcDir:      "symbols/src/"
+		srcDir:      "symbols/src/",
+		cssDir:      "css/",
+		fontsDir:    "css/fonts/",
+		jsDir:       "javascript/",
+		templateName: "Codeview",
+		templateVersion: "1.2",
+		templateLink: "http://www.thebrightlines.com/2010/05/06/new-template-for-jsdoctoolkit-codeview/"
 	};
 	
 	// is source output is suppressed, just display the links to the source file
@@ -16,25 +23,27 @@ function publish(symbolSet) {
 	}
 	
 	// create the folders and subfolders to hold the output
+	IO.mkPath((publish.conf.outDir+publish.conf.cssDir));
+	IO.mkPath((publish.conf.outDir+publish.conf.fontsDir));
+	IO.mkPath((publish.conf.outDir+publish.conf.jsDir));
 	IO.mkPath((publish.conf.outDir+"symbols/src").split("/"));
-		
+	
 	// used to allow Link to check the details of things being linked to
 	Link.symbolSet = symbolSet;
 
 	// create the required templates
 	try {
 		var classTemplate = new JSDOC.JsPlate(publish.conf.templatesDir+"class.tmpl");
-		var classesTemplate = new JSDOC.JsPlate(publish.conf.templatesDir+"allclasses.tmpl");
 	}
 	catch(e) {
 		print("Couldn't create the required templates: "+e);
 		quit();
 	}
 	
-	// some ustility filters
+	// some utility filters
 	function hasNoParent($) {return ($.memberOf == "")}
 	function isaFile($) {return ($.is("FILE"))}
-	function isaClass($) {return ($.is("CONSTRUCTOR") || $.isNamespace)}
+	function isaClass($) {return (($.is("CONSTRUCTOR") || $.isNamespace) && ($.alias != "_global_" || !JSDOC.opt.D.noGlobal))}
 	
 	// get an array version of the symbolset, useful for filtering
 	var symbols = symbolSet.toArray();
@@ -43,59 +52,50 @@ function publish(symbolSet) {
 	var files = JSDOC.opt.srcFiles;
  	for (var i = 0, l = files.length; i < l; i++) {
  		var file = files[i];
- 		var srcDir = publish.conf.outDir + "symbols/src/";
+ 		var srcDir = publish.conf.outDir + publish.conf.srcDir;
 		makeSrcFile(file, srcDir);
  	}
  	
  	// get a list of all the classes in the symbolset
- 	var classes = symbols.filter(isaClass).sort(makeSortby("alias"));
+ 	publish.classes = symbols.filter(isaClass).sort(makeSortby("alias"));
 	
 	// create a filemap in which outfiles must be to be named uniquely, ignoring case
 	if (JSDOC.opt.u) {
 		var filemapCounts = {};
 		Link.filemap = {};
-		for (var i = 0, l = classes.length; i < l; i++) {
-			var lcAlias = classes[i].alias.toLowerCase();
+		for (var i = 0, l = publish.classes.length; i < l; i++) {
+			var lcAlias = publish.classes[i].alias.toLowerCase();
 			
 			if (!filemapCounts[lcAlias]) filemapCounts[lcAlias] = 1;
 			else filemapCounts[lcAlias]++;
 			
-			Link.filemap[classes[i].alias] = 
+			Link.filemap[publish.classes[i].alias] = 
 				(filemapCounts[lcAlias] > 1)?
 				lcAlias+"_"+filemapCounts[lcAlias] : lcAlias;
 		}
 	}
 	
-	// create a class index, displayed in the left-hand column of every class page
-	Link.base = "../";
- 	publish.classesIndex = classesTemplate.process(classes); // kept in memory
-	
 	// create each of the class pages
-	for (var i = 0, l = classes.length; i < l; i++) {
-		var symbol = classes[i];
+	for (var i = 0, l = publish.classes.length; i < l; i++) {
+		var symbol = publish.classes[i];
 		
 		symbol.events = symbol.getEvents();   // 1 order matters
 		symbol.methods = symbol.getMethods(); // 2
 		
-		Link.currentSymbol= symbol;
 		var output = "";
 		output = classTemplate.process(symbol);
 		
-		IO.saveFile(publish.conf.outDir+"symbols/", ((JSDOC.opt.u)? Link.filemap[symbol.alias] : symbol.alias) + publish.conf.ext, output);
+		IO.saveFile(publish.conf.outDir+publish.conf.symbolsDir, ((JSDOC.opt.u)? Link.filemap[symbol.alias] : symbol.alias) + publish.conf.ext, output);
 	}
-	
-	// regenerate the index with different relative links, used in the index pages
-	Link.base = "";
-	publish.classesIndex = classesTemplate.process(classes);
 	
 	// create the class index page
 	try {
-		var classesindexTemplate = new JSDOC.JsPlate(publish.conf.templatesDir+"index.tmpl");
+		var classesindexTemplate = new JSDOC.JsPlate(publish.conf.templatesDir+"allclasses.tmpl");
 	}
 	catch(e) { print(e.message); quit(); }
 	
-	var classesIndex = classesindexTemplate.process(classes);
-	IO.saveFile(publish.conf.outDir, "index"+publish.conf.ext, classesIndex);
+	var classesIndex = classesindexTemplate.process(publish.classes);
+	IO.saveFile(publish.conf.outDir, (JSDOC.opt.D.index=="files"?"allclasses":"index")+publish.conf.ext, classesIndex);
 	classesindexTemplate = classesIndex = classes = null;
 	
 	// create the file index page
@@ -120,10 +120,31 @@ function publish(symbolSet) {
 
 	// output the file index page
 	var filesIndex = fileindexTemplate.process(allFiles);
-	IO.saveFile(publish.conf.outDir, "files"+publish.conf.ext, filesIndex);
+	IO.saveFile(publish.conf.outDir, (JSDOC.opt.D.index=="files"?"index":"files")+publish.conf.ext, filesIndex);
 	fileindexTemplate = filesIndex = files = null;
-}
+	
+	// copy static files
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.cssDir+"all.css", publish.conf.outDir+"/"+publish.conf.cssDir);
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.cssDir+"screen.css", publish.conf.outDir+"/"+publish.conf.cssDir);
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.cssDir+"handheld.css", publish.conf.outDir+"/"+publish.conf.cssDir);
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.jsDir+"all.js", publish.conf.outDir+"/"+publish.conf.jsDir);
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.jsDir+"html5.js", publish.conf.outDir+"/"+publish.conf.jsDir);
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.fontsDir+"mplus-1m-regular-webfont.eot", publish.conf.outDir+"/"+publish.conf.fontsDir);
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.fontsDir+"mplus-1m-regular-webfont.svg", publish.conf.outDir+"/"+publish.conf.fontsDir);
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.fontsDir+"mplus-1m-regular-webfont.ttf", publish.conf.outDir+"/"+publish.conf.fontsDir);
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.fontsDir+"mplus-1m-regular-webfont.woff", publish.conf.outDir+"/"+publish.conf.fontsDir);
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.fontsDir+"mplus-1m-bold-webfont.eot", publish.conf.outDir+"/"+publish.conf.fontsDir);
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.fontsDir+"mplus-1m-bold-webfont.svg", publish.conf.outDir+"/"+publish.conf.fontsDir);
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.fontsDir+"mplus-1m-bold-webfont.ttf", publish.conf.outDir+"/"+publish.conf.fontsDir);
+	IO.copyFile(publish.conf.templatesDir+"/"+publish.conf.fontsDir+"mplus-1m-bold-webfont.woff", publish.conf.outDir+"/"+publish.conf.fontsDir);	}
 
+/** Include a sub-template in the current template, specifying a data object */
+function subtemplate(template, data) {
+	try {
+		return new JSDOC.JsPlate(publish.conf.templatesDir+template).process(data);
+	}
+	catch(e) { print(e.message); quit(); }
+}
 
 /** Just the first sentence (up to a full stop). Should not break on dotted variable names. */
 function summarize(desc) {
@@ -142,6 +163,13 @@ function makeSortby(attribute) {
 			return 0;
 		}
 	}
+}
+
+function wordwrapNamespace(classLink) {
+	var classText = classLink.match(/[^<>]+(?=[<])/) + "";
+	var classTextNew = classText.replace(/\./g,  "<span class='break'> </span>.<span class='break'> </span>") + "";
+	classLink = classLink.replace(/[^<>]+(?=[<])/,  classTextNew);
+	return classLink;
 }
 
 /** Pull in the contents of an external file at the given path. */
@@ -191,11 +219,18 @@ function makeSignature(params) {
 
 /** Find symbol {@link ...} strings in text and turn into html links */
 function resolveLinks(str, from) {
-	str = str.replace(/\{@link ([^} ]+) ?\}/gi,
-		function(match, symbolName) {
-			return new Link().toSymbol(symbolName);
-		}
-	);
-	
-	return str;
+   str = str.replace(/\{@link ([^}]+)\}/gi,
+     function(match, symbolName) {
+	symbolName = symbolName.trim();
+	var index = symbolName.indexOf(' ');
+	if (index > 0) {
+	   var label = symbolName.substring(index + 1);
+	   symbolName = symbolName.substring(0, index);
+	   return new Link().toSymbol(symbolName).withText(label);
+	} else {
+	   return new Link().toSymbol(symbolName);
+	}
+     }
+   );
+   return str;
 }
